e33dc169c13cb992659374686ce59414cf71c339
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-03 20:49:41 +0530

    Initial commit

---
+++ src/main/java/com/hemanth/problemsolving/ProblemSolvingUsingJava8Application.java
@@ -0,0 +1,13 @@
+package com.hemanth.problemsolving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemSolvingUsingJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemSolvingUsingJava8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hemanth/problemsolving/ProblemSolvingUsingJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.problemsolving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemSolvingUsingJava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
